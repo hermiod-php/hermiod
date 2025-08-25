8046e9f780d977b189d175eb57206ff4f877e6bb
Joel Jenvey <jdjenvey@gmail.com>
2025-08-25 11:11:24 +0100

    dev: Separate development compose containers including XDebug config from limited build containers.

--- ci/build-and-test.sh
+++ ci/build-and-test.sh
@@ -4,7 +4,6 @@ set -e -x
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-docker compose -f "$SCRIPT_DIR/docker/docker-compose.yml" build
-docker compose -f "$SCRIPT_DIR/docker/docker-compose.yml" run --rm php82 composer tests-ci
-docker compose -f "$SCRIPT_DIR/docker/docker-compose.yml" run --rm php83 composer tests-ci
-docker compose -f "$SCRIPT_DIR/docker/docker-compose.yml" run --rm php84 composer tests-ci
+docker compose -f "$SCRIPT_DIR/docker/docker-compose.yml" run --rm php82-ci composer tests-ci
+docker compose -f "$SCRIPT_DIR/docker/docker-compose.yml" run --rm php83-ci composer tests-ci
+docker compose -f "$SCRIPT_DIR/docker/docker-compose.yml" run --rm php84-ci composer tests-ci
